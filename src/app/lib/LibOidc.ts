387a086edeb043263d623010a621c866c8415788
viken <viken2011@gmail.com>
2020-01-13 16:34:44 +0800

    oicd初始化修改

--- src/app/lib/LibOidc.ts
+++ src/app/lib/LibOidc.ts
@@ -1,17 +1,29 @@
-import { Issuer } from 'openid-client';
+import { Issuer,custom } from 'openid-client';
 import config from '../../config/config';
 
-// const client = (async() => {
-//   const issuer = await Issuer.discover(config.oidc.issuer);
-//   return new issuer.Client({
-//     client_id: config.oidc.client_id,
-//   });
-// })();
+custom.setHttpOptionsDefaults({
+  followRedirect: false,
+  headers: { 'User-Agent': 'koaval-curl' },
+  timeout: 5000,
+  retry: 3,
+  throwHttpErrors: false,
+});
+
+let oidcClient: any = null;
+
+export const initOidc = async () => {
+  if (oidcClient === null) {
+    const issuer = await Issuer.discover(config.oidc.issuer);
+    oidcClient = new issuer.Client({
+      client_id: config.oidc.client_id,
+    });
+  }
+};
 
 export const oidcVerify = async (token: string) => {
-  const issuer = await Issuer.discover(config.oidc.issuer);
-  const client = new issuer.Client({
-    client_id: config.oidc.client_id,
-  });
-  return await client.userinfo(token);
+  await initOidc();
+  if (!oidcClient) {
+    throw new Error('initOidc fail');
+  }
+  return await oidcClient.userinfo(token);
 };
